68e2d3dc21972acf37112436811835ebcebe03f5
HyunsuYu <lioie6478@gmail.com>
2024-11-28 15:08:33 +0900

    Init

---
+++ src/BaseFrame.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
+
+public class BaseFrame
+{
+	public BaseFrame()
+	{
+
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		BaseFrame baseFrame = new BaseFrame();
+	}
+}
